1f104c38d1b1eede0cf0a576e87cfa2a4111a989
tpomeroy747 <tpomeroy747@gmail.com>
2025-06-26 08:01:55 -0300

    Update scraper_api.py

--- scraper_api.py
+++ scraper_api.py
@@ -27,5 +27,9 @@ def scrape():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
